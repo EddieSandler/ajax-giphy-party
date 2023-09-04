5f2e5514e1bd370962a3fbcce10642b4bb0e31eb
Eddie Sandler <ed.sandler@gmail.com>
2023-09-03 21:00:49 -0400

    changed to small fixed width images

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
+"use strict"
+//define global constants
 const BASE_URL = "http://api.giphy.com/v1/gifs/search?";
 const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
-let searchTerm = '';
+
+//query all html elements for future use
+// let searchTerm = '';
 const searchForm = document.querySelector('#searchForm');
 const searchBox = document.querySelector("input[name='searchBox']");
 const searchButton = document.querySelector("#btn_search");
-const results = document.querySelector('#results');
 const images = document.querySelector('#images');
 const removeButton = document.querySelector('#btn_remove');
 
-//get search term from user input
+//event handler to get search term from user input
 searchButton.addEventListener("click", function (e) {
   e.preventDefault();
-  searchTerm = searchBox.value;
-  searchBox.value=''
+ let searchTerm = searchBox.value;
+  searchBox.value = '';
   getImageURL(searchTerm);
-
-
 });
+
 //make API call to retrieve image url
 async function getImageURL(searchTerm) {
   let url = `${BASE_URL}q=${searchTerm}&api_key=${API_KEY}`;
   let res = await axios.get(`${url}`);
-  let imageURL = res.data.data[0].images.original.url;
-  displayImage(imageURL);
+  let imageURL = res.data.data[0].images.fixed_width_small.url;
+
+  //get random number
+
+   displayImage(imageURL);
 
 }
 
@@ -32,7 +37,7 @@ function displayImage(imageURL) {
   const imageGif = document.createElement('img');
   imageGif.src = imageURL;
   images.appendChild(imageGif);
-  searchTerm = ' ';
+  let searchTerm = ' ';
   images.appendChild(imageGif);
 
 }
